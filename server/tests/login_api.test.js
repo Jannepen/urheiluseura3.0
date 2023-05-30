9a6c4f6f995f1d8a279aa4b5049bc42db959d737
Penttala Janne O <jannepen@cubbli18-gold.local>
2023-05-30 13:45:27 +0300

    env change

--- server/tests/login_api.test.js
+++ server/tests/login_api.test.js
@@ -32,8 +32,8 @@ test('existing user can log in', async () => {
     await api
         .post('/api/login')
         .send(existingUser)
-        .expect(response => {console.log(response)})
-        .expect(500)
+//        .expect(response => {console.log(response)})
+        .expect(200)
 
     const users = await User.findAll()
     expect(users.length).toBe(1)
